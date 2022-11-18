85d071b75017a1ecab3313b3bf0706cce434b309
randomusernamea <stars.a@outlook.com>
2022-11-17 22:25:36 -0300

    Agrego Comps

--- desafio-react-1-1/src/App.js
+++ desafio-react-1-1/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import FooterComp from './Componentes/FooterComp.jsx'
+import MainComp from './Componentes/MainComp.jsx'
+import HeadComp from './Componentes/HeadComp.jsx'
+
 
 function App() {
   return (
     <div className="App">
-     
+     <HeadComp/>
+     <MainComp/>
+     <FooterComp/>
     </div>
   );
 }
