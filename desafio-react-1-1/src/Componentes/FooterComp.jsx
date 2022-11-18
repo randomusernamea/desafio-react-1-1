fbcaf728d1f38d3e9fb79dc50769e4d852d7a336
randomusernamea <stars.a@outlook.com>
2022-11-17 22:25:43 -0300

    &^

---
+++ desafio-react-1-1/src/Componentes/FooterComp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function FooterComp(props){
+    return(
+        <div></div>
+    );
+
+}
+
+
+export default FooterComp;
---
+++ desafio-react-1-1/src/Componentes/HeadComp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function HeadComp(props){
+    return(
+        <div></div>
+    );
+
+}
+
+
+export default HeadComp;
